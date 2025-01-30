c28f74685e4b2ed048f710286b77a8fb5cd00513
PavlovIvan1 <144616500+PavlovIvan1@users.noreply.github.com>
2025-01-30 20:11:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 
-
 class Video:
     def __init__(self, path):
         self.cap = cv2.VideoCapture(path)
@@ -9,12 +8,12 @@ class Video:
             success, img = self.cap.read()
             cv2.imshow('Video', img)
 
-            if cv2.waitKey(15) & 0xFF == ord('q'):  # время показа 1 кадра = waitkey
+            if cv2.waitKey(15) & 0xFF == ord('q'):
                 break
 
 
 def main():
-    capture = Video(0) # there is 0 it is index of webcam, you can use path to a video like "video.mp4"
+    capture = Video(0)
     print(capture)
 
 
